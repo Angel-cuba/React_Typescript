b38a7f55b30bcdd1d2c52bce2e5e26f9ab5ec96d
Mac_Developer <araozangel842@gmail.com>
2022-03-06 16:29:01 +0200

    Bootswatch to styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Task {
+	name: string;
+	done: boolean;
+}
+
+function App(): JSX.Element {
+	const [newTask, setNewTask] = useState<string>('');
+	const [tasks, setTasks] = useState<Task[]>([]);
+
+	const handleTask = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		addTask(newTask);
+		setNewTask('');
+	};
+
+	const addTask = (name: string) => {
+		const newTasks = [...tasks, { name, done: false }];
+		setTasks(newTasks);
+	};
+
+	return (
+		<div className="container p-4">
+			<div className="row">
+				<div className="col-md-6 offset-md-3">
+					<div className="card">
+						<div className="card-body">
+							<form onSubmit={handleTask}>
+								<input
+									type="text"
+									onChange={(e) => setNewTask(e.target.value)}
+									value={newTask}
+									className="form-control"
+								/>
+								<button className="btn btn-success btn-block mt-2">Save</button>
+							</form>
+						</div>
+					</div>
+					{tasks.map((task: Task, index: number) => (
+						<h1 key={index}>{task.name}</h1>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
